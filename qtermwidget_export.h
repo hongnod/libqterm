8c4cb1ae1d13ecbe87b54d4274b45cec2b9082f2
topillar <topillar@gmail.com>
2018-06-25 00:16:40 +0800

    first upload.

---
+++ qtermwidget_export.h
@@ -0,0 +1,5 @@
+
+
+
+#define QTERMWIDGET_EXPORT __declspec(dllexport)
+
---
+++ wcwidth.h
@@ -0,0 +1,11 @@
+#ifndef WCWIDTH_H_INCLUDED
+#define WCWIDTH_H_INCLUDED
+
+#include <stdlib.h>
+
+
+
+int wcwidth(wchar_t ucs);
+
+
+#endif
