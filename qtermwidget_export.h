b6492809682f6b7037d56fe57b3e4930b85fbd9e
topillar <topillar@qq.com>
2018-06-26 16:02:52 +0800

    renew export

--- qtermwidget_export.h
+++ qtermwidget_export.h
@@ -1,12 +1,47 @@
-﻿#ifndef _Q_TERM_WIDGET
-#define _Q_TERM_WIDGET
+
+#ifndef QTERMWIDGET_EXPORT_H
+#define QTERMWIDGET_EXPORT_H
+
+#ifdef QTERMWIDGET5_STATIC_DEFINE
+#  define QTERMWIDGET_EXPORT
+#  define QTERMWIDGET5_NO_EXPORT
+#else
+#  ifndef QTERMWIDGET_EXPORT
+#    ifdef qtermwidget5_EXPORTS
+        /* We are building this library */
+#      define QTERMWIDGET_EXPORT __declspec(dllexport)
+#    else
+        /* We are using this library */
+#      define QTERMWIDGET_EXPORT __declspec(dllimport)
+#    endif
+#  endif
+
+#  ifndef QTERMWIDGET5_NO_EXPORT
+#    define QTERMWIDGET5_NO_EXPORT 
+#  endif
 #endif
 
-#define QTERMWIDGET_EXPORT __declspec(dllexport)
+#ifndef QTERMWIDGET5_DEPRECATED
+#  define QTERMWIDGET5_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef QTERMWIDGET5_DEPRECATED_EXPORT
+#  define QTERMWIDGET5_DEPRECATED_EXPORT QTERMWIDGET_EXPORT QTERMWIDGET5_DEPRECATED
+#endif
+
+#ifndef QTERMWIDGET5_DEPRECATED_NO_EXPORT
+#  define QTERMWIDGET5_DEPRECATED_NO_EXPORT QTERMWIDGET5_NO_EXPORT QTERMWIDGET5_DEPRECATED
+#endif
+
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef QTERMWIDGET5_NO_DEPRECATED
+#    define QTERMWIDGET5_NO_DEPRECATED
+#  endif
+#endif
+
+#endif /* QTERMWIDGET_EXPORT_H */
 
 #define TRANSLATIONS_DIR  "./translations"
 
 #define KB_LAYOUT_DIR "./kb-layouts"
 #define COLORSCHEMES_DIR "./color-schemes"
-
-
